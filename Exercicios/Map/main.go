1e584533f6b45aaf748652a0b07c94ac93e700ba
Marcelo Manfredini <manfredini1968@gmail.com>
2025-10-14 11:33:45 -0300

    Map e Func

--- Exercicios/Map/main.go
+++ Exercicios/Map/main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
+		"vermelho": "#ff0000",
+		"verde":    "#00ff00",
+		"azul":     "#0000ff",
 	}
-	//colors["white"] = "#ffffff"
-	//delete(colors, "red")
+	//colors["branco"] = "#ffffff"
+	//delete(colors, "vermelho")
 	//fmt.Println(colors)
 	printMap(colors)
 }
