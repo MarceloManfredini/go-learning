df013745076610cfb99e8940ff684eb5e66ac157
Marcelo Manfredini <manfredini1968@gmail.com>
2025-10-14 11:29:32 -0300

    Http.get e print do resultado

--- Exercicios/Http/main.go
+++ Exercicios/Http/main.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://google.com")
+	resposta, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
-	resp.Body.Close()
+	io.Copy(os.Stdout, resposta.Body)
+	resposta.Body.Close()
 
 	//fmt.Println(resp)
 	/* 	bs := make([]byte, 99999)
